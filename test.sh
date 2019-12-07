40844503301b8042ed8ad6da60f208ff19185efb
Oscar Yu <yuszy@outlook.com>
2019-12-08 00:06:14 +1100

    fixed typo in test

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl POST -d '{"msg": "Hello service"}' -H "Content-Type: application/json" http://0.0.0.0:80/greet/hello
+curl -X POST -d '{"Msg": "Hello service"}' -H "Content-Type:application/json" http://0.0.0.0:80/greet/hello
