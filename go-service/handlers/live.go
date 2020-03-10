0924922dbcbdaece2e2872bd34cb23c3c7a6dde3
Oscar Yu <yuszy@outlook.com>
2020-03-10 20:39:42 +1100

    updated go service and added server stream api

---
+++ go-service/handlers/live.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	contracts "go-service/contracts/greeter-go"
+	"time"
+)
+
+// GetLiveData - Push live data to client
+func (*Service) GetLiveData(req *contracts.LiveDataRequest, srv contracts.Greeter_GetLiveDataServer) error {
+	for i := 0; ; i++ {
+		srv.Send(&contracts.LiveDataReply{
+			Data: (int32)(i),
+		})
+		time.Sleep(1000000)
+	}
+
+	return nil
+}
