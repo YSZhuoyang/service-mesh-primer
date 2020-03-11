fd8f73bc2e01ff503505d05ba5b11d8cdf9d79ff
Oscar Yu <yuszy@outlook.com>
2020-03-11 18:18:47 +1100

    fixed some web client issues

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -4,8 +4,10 @@ const { GreeterClient } = require('./greeter-go/greet_go_grpc_web_pb.js');
 var echoService = new GreeterClient('http://localhost:80');
 
 function getLiveData() {
-    echoService.echo(new LiveDataRequest(), {}, function (err, response) {
+    echoService.getLiveData(new LiveDataRequest(), {}, function (err, response) {
         console.log("Received: " + response.getData());
         document.getElementById("data").textContent = response.getData();
     });
 }
+
+document.getElementById("send").onclick = getLiveData;
