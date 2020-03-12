73f14d37a0a3ad8aa59ae76cd0d9d9f5a5962e93
Oscar Yu <yuszy@outlook.com>
2020-03-12 22:38:32 +1100

    web client works

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -1,12 +1,27 @@
-const { LiveDataRequest, LiveDataReply } = require('./greeter-go/greet_go_pb.js');
+const { LiveDataRequest, HelloRequest, LiveDataReply } = require('./greeter-go/greet_go_pb.js');
 const { GreeterClient } = require('./greeter-go/greet_go_grpc_web_pb.js');
 
 var echoService = new GreeterClient('http://localhost:80');
 
-function getLiveData() {
-    echoService.getLiveData(new LiveDataRequest(), {}, function (err, response) {
+const getLiveData = () => {
+    const req = new LiveDataRequest();
+    const stream = echoService.getLiveData(req, {});
+    stream.on('data', function (response) {
         console.log("Received: " + response.getData());
         document.getElementById("data").textContent = response.getData();
+    });
+    stream.on('status', function (status) {
+        console.log("Received status code: " + status);
+        if (status.metadata) {
+            console.log("Received metadata");
+            console.log(status.metadata);
+        }
+    });
+    stream.on('error', function (err) {
+        console.log('Error code: ' + err.code + ' "' + err.message + '"');
+    });
+    stream.on('end', function () {
+        console.log("stream end signal received");
     });
 }
 
