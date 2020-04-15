b13fe95310c1323477372349f3ee067c95ed9ee2
Oscar Yu <yuszy@outlook.com>
2020-04-15 23:31:17 +1000

    updated tests

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # HTTP1.1
-curl -X POST -d '{"Msg": "Hello go service"}' -H "Content-Type:application/json" http://0.0.0.0:80/greet/hello
-curl -X POST -d '{"Msg": "Hello dotnet service"}' -H "Content-Type:application/json" http://0.0.0.0:80/greet2/hello
+curl --insecure -X POST -d '{"Msg": "Hello go service"}' -H "Content-Type:application/json" https://stock-discovery-oyu.space:443/greet/hello
+# curl -X POST -d '{"Msg": "Hello dotnet service"}' -H "Content-Type:application/json" http://0.0.0.0:80/greet2/hello
 
 # HTTP2
-curl --http2-prior-knowledge -X POST -d '{"Msg": "Hello go service"}' -H "Content-Type:application/json" http://0.0.0.0:80/greet/hello
-curl --http2-prior-knowledge -X POST -d '{"Msg": "Hello dotnet service"}' -H "Content-Type:application/json" http://0.0.0.0:80/greet2/hello
+curl --insecure --http2-prior-knowledge -X POST -d '{"Msg": "Hello go service"}' -H "Content-Type:application/json" https://stock-discovery-oyu.space:443/greet/hello
+# curl --http2-prior-knowledge -X POST -d '{"Msg": "Hello dotnet service"}' -H "Content-Type:application/json" http://0.0.0.0:80/greet2/hello
